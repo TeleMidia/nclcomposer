b273cedd753e7b202a97ed1a20cd083f2f6bd493
robertogerson <robertogerson@telemidia.puc-rio.br>
2011-06-06 17:06:40 -0300

    [core] maint: Add doxygen format doc.
    * core/include/util/AttributeReferences.h: Likewise.

--- composer-core/core/include/util/AttributeReferences.h
+++ composer-core/core/include/util/AttributeReferences.h
@@ -2,26 +2,59 @@
 #define ATTRIBUTEREFERENCES_H
 
 #include <QString>
-/**
-  */
 
 namespace composer {
         namespace util {
 
+/*!
+ \brief Keep information about which pair (element, attribute) can
+            refer a value of another pair (element, attribute).
+
+ This class is mainly used to described the language profile.
+*/
 class AttributeReferences {
 private:
-    QString element, attr,
-    ref_element, ref_attr;
+    QString element, attr, /*!< TODO */
+    ref_element, ref_attr; /*!< TODO */
 
 public:
+    /*!
+     \brief Constructor.
+
+     \param element The source Element.
+     \param attr The source Attribute (should be an element's attribute).
+     \param ref_element The target element.
+     \param ref_attr The target attribute (should be an ref_element's
+                        attribute).
+    */
     AttributeReferences ( QString element,
                           QString attr,
                           QString ref_element,
                           QString ref_attr);
 
+    /*!
+     \brief Get the source element.
+
+     \return QString the name of the source element.
+    */
     QString getElement();
+    /*!
+     \brief Get the source attribute.
+
+     \return QString the name of the source attribute.
+    */
     QString getAttribute();
+    /*!
+     \brief Get the name of referred element.
+
+     \return QString The name of the referred element.
+    */
     QString getRefElement();
+    /*!
+     \brief Get the name of the referred attribute (an ref_element's attribute).
+
+     \return QString The name of the referred attribute.
+    */
     QString getRefAttribute();
 };
 
