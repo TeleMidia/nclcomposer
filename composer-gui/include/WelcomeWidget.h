07b9cca3f9af66681c78f77915df7ad12f75b014
Roberto Azevedo <robertogerson@telemidia.puc-rio.br>
2011-09-02 18:27:46 -0400

    [gui] maint: Loading NCL Club application using RSS.
    * include/WelcomeWidget.h: Likewise.
    * src/WelcomeWidget.cpp: Likewise.
    * ui/WelcomeWidget.ui: Likewise.
    * images.qrc: Add images to next and previous tips.
    * images/left_arrow.png: New file.
    * images/right_arrow.png: New file.

--- composer-gui/include/WelcomeWidget.h
+++ composer-gui/include/WelcomeWidget.h
@@ -2,17 +2,24 @@
 #define WELCOMEWIDGET_H
 
 #include <QObject>
+#include <QDebug>
 #include <QEvent>
 #include <QWidget>
 #include <QTableWidget>
 
+#include <QXmlStreamReader>
+#include <QtNetwork>
+#include <QUrl>
+#include <QDesktopServices>
+
+#define NCL_CLUB_URL "http://club.ncl.org.br/rss.xml"
 
 namespace Ui{
 class WelcomeWidget;
 }
 
 namespace composer {
-    namespace gui {
+namespace gui {
 
 /* FROM:
 http://www.streamreader.org/stackoverflow/questions/2480773/qt-styling-qtabwidget
@@ -54,35 +61,49 @@ public:
     */
     virtual ~WelcomeWidget();
 
+    /*!
+      \brief Set the maximum items that will be showed by this WelcomeWidget
+
+      \param maxItems the number of maximum items;
+     */
+    void setMaximumItems(int maxItems);
+
+    /*!
+      \brief Returns the maximum number of items that will be showed by this
+        WelcomeWidget
+     */
+    int getMaximumItems();
+
 private slots:
     void on_commandLinkButton_29_clicked();
-
     void on_commandLinkButton_6_clicked();
-
     void on_commandLinkButton_9_clicked();
-
     void on_commandLinkButton_30_clicked();
-
     void on_commandLinkButton_10_clicked();
-
     void on_commandLinkButton_11_clicked();
-
     void on_commandLinkButton_31_clicked();
-
     void on_commandLinkButton_27_clicked();
-
     void on_commandLinkButton_26_clicked();
-
     void on_commandLinkButton_25_clicked();
-
     void on_commandLinkButton_28_clicked();
-
     void on_commandLinkButton_7_clicked();
-
     void on_commandLinkButton_8_clicked();
 
 private:
+    int connectionId;
+    QHttp http;
+    QXmlStreamReader xmlReader;
+    int n_items;
+    int maxItems;
+
     Ui::WelcomeWidget *ui;
+
+    void loadRSS();
+    void parseXml();
+
+private slots:
+    void readData(const QHttpResponseHeader &resp);
+    void finishRSSLoad(int, bool);
 };
 
 } }
--- composer-gui/src/WelcomeWidget.cpp
+++ composer-gui/src/WelcomeWidget.cpp
@@ -1,22 +1,161 @@
 #include <WelcomeWidget.h>
 #include <ui_WelcomeWidget.h>
 
-#include <QUrl>
-#include <QDesktopServices>
-
 namespace composer {
-    namespace gui {
+namespace gui {
 
 WelcomeWidget::WelcomeWidget(QWidget *parent): QWidget(parent),
-                                                   ui(new Ui::WelcomeWidget)
+    ui(new Ui::WelcomeWidget)
 {
     ui->setupUi(this);
     ui->tabWidget->installEventFilter(new ResizeFilter(ui->tabWidget));
+
+    this->maxItems = 5;
+    this->n_items = 0;
+
+    //Connect the QHttp with
+    connect(&http, SIGNAL(readyRead(const QHttpResponseHeader &)),
+            this, SLOT(readData(const QHttpResponseHeader &)));
+
+    connect(&http, SIGNAL(requestFinished(int,bool)),
+            this, SLOT(finishRSSLoad(int, bool)));
+
+    loadRSS();
 }
 
 WelcomeWidget::~WelcomeWidget()
 {
+    delete ui;
+}
 
+void WelcomeWidget::loadRSS()
+{
+    xmlReader.clear();
+
+    QUrl url = QUrl::fromUserInput(NCL_CLUB_URL);
+
+    http.setHost(url.host());
+    connectionId = http.get(url.path());
+}
+
+void WelcomeWidget::readData(const QHttpResponseHeader &resp)
+{
+    if (resp.statusCode() != 200)
+    {
+        http.abort();
+    }
+    else
+    {
+        QByteArray bytes = http.readAll();
+        xmlReader.addData(bytes);
+        parseXml();
+    }
+}
+
+void WelcomeWidget::parseXml()
+{
+    QString currentTag;
+    QString linkString;
+    QString titleString;
+    QString dateString;
+
+    qDebug() << "WelcomeWidget::parseXml()";
+
+    bool readingItem = false;
+
+    while (!xmlReader.atEnd())
+    {
+        xmlReader.readNext();
+
+        if (xmlReader.isStartElement())
+        {
+            if (xmlReader.name() == "item")
+            {
+                readingItem = true;
+
+                linkString.clear();
+                titleString.clear();
+                dateString.clear();
+            }
+
+            currentTag = xmlReader.name().toString();
+        }
+        else if (xmlReader.isEndElement())
+        {
+            if (xmlReader.name() == "item")
+            {
+                if(n_items < this->maxItems && readingItem)
+                {
+
+                    qDebug() << "#########" << titleString
+                             << linkString << dateString << n_items;
+                    //TODO: Update UI
+                    QCommandLinkButton *button =
+                            new QCommandLinkButton(ui->frame_ClubeNCL);
+                    button->setText(titleString);
+
+                    ui->frame_ClubeNCL->layout()->addWidget(button);
+                    n_items++;
+                    readingItem = false;
+                }
+
+                titleString.clear();
+                linkString.clear();
+                dateString.clear();
+            }
+
+        }
+        else if (xmlReader.isCharacters() && !xmlReader.isWhitespace())
+        {
+            if (currentTag == "title" && readingItem)
+                titleString += xmlReader.text().toString();
+            else if (currentTag == "link")
+                linkString += xmlReader.text().toString();
+            else if (currentTag == "pubDate")
+                dateString += xmlReader.text().toString();
+        }
+    }
+
+    if (xmlReader.error() &&
+            xmlReader.error() != QXmlStreamReader::PrematureEndOfDocumentError)
+    {
+        qWarning() << "XML ERROR:"
+                   << xmlReader.lineNumber() << ": " << xmlReader.errorString();
+
+        http.abort();
+    }
+}
+
+void WelcomeWidget::setMaximumItems(int maxItems)
+{
+    this->maxItems = maxItems;
+}
+
+int WelcomeWidget::getMaximumItems()
+{
+    return maxItems;
+}
+
+void WelcomeWidget::finishRSSLoad(int connectionId, bool error)
+{
+    qDebug() << "finishRSSLoad(" << connectionId << "," << error << ");";
+    if(!error)
+    {
+        if(this->connectionId == connectionId)
+        {
+            QCommandLinkButton *button =
+                    new QCommandLinkButton(this);
+            button->setIconSize(QSize(0,0));
+
+            ui->frame_ClubeNCL->layout()->addWidget(button);
+
+            button->setText(tr("More..."));
+        }
+    }
+    else
+    {
+        qDebug() << "Connection to NCL Club fails";
+    }
 }
 
 } }
