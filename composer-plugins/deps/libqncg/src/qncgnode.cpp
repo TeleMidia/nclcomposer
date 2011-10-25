b61b92241ce770c3ef75fb34fe2f4a594ca3c9e0
Roberto Azevedo <robertogerson@telemidia.puc-rio.br>
2011-10-25 16:03:56 -0200

    [plugins] maint: Update License to LGPL.

--- composer-plugins/deps/libqncg/src/qncgnode.cpp
+++ composer-plugins/deps/libqncg/src/qncgnode.cpp
@@ -1,11 +1,19 @@
-/* Copyright (c) 2011 Telemidia/PUC-Rio.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *    Telemidia/PUC-Rio - initial API and implementation
+/*
+ * Copyright 2011 TeleMidia/PUC-Rio.
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either 
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this library.  If not, see
+ * <http://www.gnu.org/licenses/>. 
  */
 #include "qncgnode.h"
 
