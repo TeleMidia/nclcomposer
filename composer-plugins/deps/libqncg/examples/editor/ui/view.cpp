efbead69ded917aac75b26bda129b9d18852ccc6
Eduardo Cruz Araújo <edcaraujo@telemida.puc-rio.br>
2011-11-28 19:42:35 -0200

    [plugins] maint: adding 'editor' example for libqncg.
    * libqncg/examples/editor/editor.pro: likewise.
    * libqncg/examples/editor/main.cpp: likewise.
    * libqncg/examples/editor/ui/mainwindow.cpp: likewise.
    * libqncg/examples/editor/ui/mainwindow.h: likewise.
    * libqncg/examples/editor/ui/node.cpp: likewise.
    * libqncg/examples/editor/ui/node.h: likewise.
    * libqncg/examples/editor/ui/scene.cpp: likewise.
    * libqncg/examples/editor/ui/scene.h: likewise.
    * libqncg/examples/editor/ui/view.cpp: likewise.
    * libqncg/examples/editor/ui/view.h: likewise.

---
+++ composer-plugins/deps/libqncg/examples/editor/ui/view.cpp
@@ -0,0 +1,64 @@
+#include "view.h"
+
+View::View(QWidget* parent)
+    : QGraphicsView(parent)
+{
+    createObjects();
+    createConnections();
+
+    n = 0;
+
+    selected = NULL;
+}
+
+View::~View()
+{
+
+}
+
+void View::createObjects()
+{   
+    scene = new Scene();
+    scene->setParent(this);
+    scene->setSceneRect(0, 0, 2000, 2000);
+
+    setScene(scene);
+}
+
+void View::createConnections()
+{
+    connect(scene, SIGNAL(entityChanged(QncgGraphicsEntity*)), SLOT(alertEntityChange(QncgGraphicsEntity*)));
+    connect(scene, SIGNAL(entitySelected(QncgGraphicsEntity*)), SLOT(alertEntitySelection(QncgGraphicsEntity*)));
+}
+
+void View::alertEntityChange(QncgGraphicsEntity* entity)
+{
+    qDebug() << "[QNCG]" << "[" << ++n << "]" << ":" << "Entity changed.";
+}
+
+void View::alertEntitySelection(QncgGraphicsEntity* entity)
+{
+    qDebug() << "[QNCG]" << "[" << ++n << "]" << ":" << "Entity selected.";
+
+    if (selected != NULL){
+        selected->setSelected(false);
+    }
+
+    selected = entity;
+    selected->setSelected(true);
+}
+
+void View::mousePressEvent(QMouseEvent* event)
+{
+    QGraphicsView::mousePressEvent(event);
+
+    if (event->button() == Qt::LeftButton){
+        if (!event->isAccepted()){
+            if (selected != NULL){
+                selected->setSelected(false);
+            }
+
+            selected = NULL;
+        }
+    }
+}
---
+++ composer-plugins/deps/libqncg/examples/editor/ui/view.h
@@ -0,0 +1,40 @@
+#ifndef VIEW_H
+#define VIEW_H
+
+#include <QGraphicsView>
+#include <QMouseEvent>
+
+#include <QDebug>
+
+#include "scene.h"
+
+class View : public QGraphicsView
+{
+    Q_OBJECT
+
+public:
+    View(QWidget* parent = 0);
+
+    ~View();
+
+protected:
+    virtual void mousePressEvent(QMouseEvent* event);
+
+protected slots:
+    void alertEntityChange(QncgGraphicsEntity* entity);
+
+    void alertEntitySelection(QncgGraphicsEntity* entity);
+
+private:
+    void createObjects();
+
+    void createConnections();
+
+    Scene* scene;
+
+    int n;
+
+    QncgGraphicsEntity* selected;
+};
+
+#endif // VIEW_H
