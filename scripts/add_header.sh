b063ae360edbfd22d2d87b2cb983054cbb84bf1f
Roberto Azevedo <robertogerson@telemidia.puc-rio.br>
2011-11-29 16:44:51 -0200

    [all] maint: Update TODO and script to generate header.
    * TODO: Likewise.
    * scripts/add_header.sh: Likewise.
    * scripts/header_lgpl.conf: New file.

--- scripts/add_header.sh
+++ scripts/add_header.sh
@@ -2,27 +2,51 @@
 # add_header.sh -- Adds a header to all CPP/H files on a given path.
 # This is a bash-only script.
 #
-
 PROGRAM_NAME='build.sh'
 PROGRAM_VERSION='1.0'
+PWD=`pwd`
 AUTHORS='Roberto Gerson Azevedo'
 BUGS_TO='robertogerson@telemidia.puc-rio.br'
 
+ISTEST=0         # You should turn this variable to 0 when really want to apply
+                 # the changes.
+FIND_PATH=$1     # Where to find the files?
+LICENSE_FILE=$2  # Which header content to put in the file?
+                 # This must be a text file
 
-files=`find . -path './composer-plugins/ncl-text-editor/deps' -prune -o -path './composer-plugins/content-provider' -prune -o -name '*.h' -or -name '*.cpp'`
+#if license file doesn't exist do nothing!
+if [ -f $LICENSE_FILE ]; then
+  echo "LICENSE FILE DOESN'T EXIST!"
+  exit
+fi
+
+files=`find $1 -path '*ncl-text-editor/deps' -prune -o -path \
+      '*content-provider' -prune -o -path '*composer-gui/deps' -prune -o -name '*.h' -or -name '*.cpp'`
+
 
 #for each file add the head form head.conf
 for file in $files; do
   echo $file
   
-  # Test if the License HEAD is Already on File.
+  # Test if the License HEAD is Already on the file.
   if head $file -n 9 | grep "Eclipse Public License" >/dev/null; then 
-    echo "... File Already with License"; 
-    echo "... Removing previous License"; 
-    sed -i '1,9d' $file
-  fi 
-  echo ".. Adding LICENSE HEADER."
-  cat scripts/header_epl.conf >/tmp/file
-  cat $file >> /tmp/file
-  cp /tmp/file $file
+    echo "... File Already with EPL License"; 
+    echo "... Removing previous EPL License";
+    if [ $ISTEST -eq 0 ]; then
+      sed -i '1,9d' $file
+    fi
+  elif head $file -n 9 | grep "" >/dev/null; then
+    echo "... File Already with LGPL License"
+    echo "... Removing LGPL License"
+    if [ $ISTEST -eq 0 ]; then
+      sed -i '1,17d' $file
+    fi
+  fi
+  if [ $ISTEST -eq 0 ]; then   
+    echo ".. Adding $LICENSE_FILE LICENSE HEADER."
+    cat ./scripts/$LICENSE_FILE >/tmp/file
+    cat $file >> /tmp/file
+    cp /tmp/file $file
+  fi
 done
+
