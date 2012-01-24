8f0c5ba1d35a64cc0a80f239db6bc1a5a19174d9
Roberto Azevedo <robertogerson@telemidia.puc-rio.br>
2012-01-24 14:39:53 -0200

    [all] maint: Add User Manual doxygen source.
    * Doxyfile: Likewise.
    * README: Likewise.
    * TODO: Likewise.
    * user_manual/Doxyfile: New file.
    * user_manual/DoxygenLayout.xml: New file.
    * user_manual/doxygen.css: New file.
    * user_manual/user_manual.cpp: New file.

---
+++ user_manual/user_manual.cpp
@@ -0,0 +1,86 @@
+/*!
+\mainpage Main Page
+\image html nclcomposer-full-reflection.png Version: 0.1.0
+This is the user manual of NCL Composer. This manual is divided into the following sections:
+
+- \subpage intro
+  - \ref helloworld
+
+- \subpage views
+  - \ref textualview
+  - \ref outlineview
+  - \ref structuralview
+  - \ref layoutview 
+  - \ref propertiesview
+  - \ref validator
+
+- \subpage customize
+  - \ref perspectives
+  - \ref installplugins
+
+- \subpage running
+  - \ref rungingavm
+  - \ref runlocalginga
+
+- \subpage tuto
+  - \ref tuto1
+
+- \subpage faq
+  - \ref faq1
+  - \ref faq2
+
+- \subpage getinvolved
+  - \ref reportabug
+
+- \subpage advanced
+  - \ref devplugins
+
+\section sec_copyright Copyright Policy
+NCL Composer has been developed by Telemidia/PUC-Rio since 2011 and available
+under <a target='_blank' href='http://eclipse.org/legal/epl-v10.html'>
+Eclipse Public License</a>. Together with the NCL Composer core, we also
+develop some default plugins that are available under LGPL.
+
+\page intro Introduction to NCL Composer
+- \ref helloworld
+
+\section helloworld Your first project
+
+\page views Views: Editing your project
+\section textualview Textual View
+
+\section outlineview Outline View
+
+\section structuralview Structural View
+
+\section layoutview Layout View
+
+\section propertiesview Properties View
+
+\section validator Validator
+
+\page customize Customize
+\section perspectives Perspectives: Organizing your interface
+
+\section installplugins Installing new plugins
+
+\page running Running your project
+
+\section rungingavm Ginga Virtual Set-top box
+
+\section runlocalginga Ginga local player
+
+\page faq FAQ
+\section faq1 FAQ 1
+\section faq2 FAQ 2
+
+\page tuto Tutorials
+\section tuto1 Playing a video
+
+\page getinvolved Get Involved
+  \section reportabug Report a bug
+  \section developersmanual Developer's manual
+
+\page advanced Advanced
+\section devplugins Developing your own plugins
+*/
