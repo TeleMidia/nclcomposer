6d135a109016dec1c30981cb72548ddb67903411
Roberto Azevedo <robertogerson@telemidia.puc-rio.br>
2012-02-07 20:14:05 -0200

    [all] doc: Little changes in the user manual.
    * user_manual/user_manual.cpp: Likewise.
    * README: Update with deb-package.sh script.
    * user_manual/Doxyfile: Fix english problems.

--- user_manual/user_manual.cpp
+++ user_manual/user_manual.cpp
@@ -1,10 +1,26 @@
 /*!
 \mainpage Main Page
 \image html nclcomposer-full-reflection.png Version: 0.1.0
-This is the user manual of NCL Composer. This manual is divided into the following sections:
+\section Introduction
+NCL Composer is a multiplataform and extensible tool that helps you to create
+and manipulate Nested Context Language documents for interactive digital TV.
+
+It can help you in many ways:
+- Providing autocomplete and validation features to text-based authoring.
+- Providing graphical abstraction over NCL source code aiming to easy the
+  authorship for non-programmers (e.g a structural and layout representation of
+  the document).
+- Providing transmition plugins that easy the task to send your application
+  over the air or over an IPTV network. 
+
+This manual is divided into two main parts, each of which is divided into
+several sections.
+
+The first part is the \b User \b Manual:
 
 - \subpage intro
-  - \ref helloworld
+  - \ref installation
+  - \ref gettingstarted
 
 - \subpage views
   - \ref textualview
@@ -22,9 +38,6 @@ This is the user manual of NCL Composer. This manual is divided into the followi
   - \ref rungingavm
   - \ref runlocalginga
 
-- \subpage tuto
-  - \ref tuto1
-
 - \subpage faq
   - \ref faq1
   - \ref faq2
@@ -32,8 +45,11 @@ This is the user manual of NCL Composer. This manual is divided into the followi
 - \subpage getinvolved
   - \ref reportabug
 
-- \subpage advanced
-  - \ref devplugins
+The second part provides information for developers that wants to extend the NCL
+Composer features:
+
+- \subpage devplugins
+- \subpage translateplugins
 
 \section sec_copyright Copyright Policy
 NCL Composer has been developed by Telemidia/PUC-Rio since 2011 and available
@@ -42,9 +58,17 @@ Eclipse Public License</a>. Together with the NCL Composer core, we also
 develop some default plugins that are available under LGPL.
 
 \page intro Introduction to NCL Composer
-- \ref helloworld
+- \ref installation
+- \ref gettingstarted
 
-\section helloworld Your first project
+\section installation Installation
+\subsection Windows
+TODO
+\subsection Linux
+TODO
+\subsection MACOS
+TODO
+\section gettingstarted Getting started
 
 \page views Views: Editing your project
 \section textualview Textual View
@@ -81,6 +105,6 @@ develop some default plugins that are available under LGPL.
   \section reportabug Report a bug
   \section developersmanual Developer's manual
 
-\page advanced Advanced
-\section devplugins Developing your own plugins
+\page devplugins Developing your own plugins
+\page translateplugins Translating your plugins
 */
