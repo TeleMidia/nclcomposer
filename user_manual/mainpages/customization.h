08d32be6d30deb8d7d4b010dcd39074e0bb7704f
Roberto Azevedo <robertogerson@telemidia.puc-rio.br>
2012-02-10 20:08:08 -0200

    [all] maint: Reorganization of User Manual.
    * user_manual/Doxyfile: Likewise. We are now following the same organization of
      wxWidget manuals.
    * user_manual/doxygen.css: Likewise.
    * user_manual/images/plugins.png: New file.
    * user_manual/images/welcome_widget.png: New file.
    * user_manual/mainpages/customization.h: New file.
    * user_manual/mainpages/install.h: New file.
    * user_manual/mainpages/interface.h: New file.
    * user_manual/mainpages/mainpage.h: New file.
    * user_manual/mainpages/run.h: New file.
    * user_manual/mainpages/views.h: New file.
    * user_manual/user_manual.cpp: Deleted.

---
+++ user_manual/mainpages/customization.h
@@ -0,0 +1,7 @@
+/**
+\page page_customization Personalização
+\section perspectives Organizando a sua interface
+
+\section installplugins Instalação de novos plugins
+
+*/
---
+++ user_manual/mainpages/interface.h
@@ -0,0 +1,37 @@
+/**
+
+\page page_interface Interface do NCL Composer
+
+\li \ref page_interface_welcome
+\li \ref page_interface_menus
+\li \ref page_interface_toolbar
+\li \ref page_interface_allplugins
+
+<hr/>
+
+
+
+\section page_interface_welcome Tela de Boas Vindas
+Ao iniciar o NCL Composer, a tela inicial é uma tela de boas vindas:
+
+\image html welcome_widget.png "Tela de Boas Vindas" width=2cm
+
+
+
+
+\section page_interface_menus Menus
+TODO
+
+
+
+
+\section page_interface_toolbar Barra de ações
+TODO
+
+
+
+\section page_interface_allplugins Visões
+
+\image html plugins.png
+
+*/
