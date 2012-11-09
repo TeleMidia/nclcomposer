d2971401d28fe1c1b42792b9a0b1469b80ccc1b2
Roberto Azevedo <robertogerson@telemidia.puc-rio.br>
2012-11-09 16:53:46 -0200

    [all] maint: Update image sizes for a better pdf result.
    * generate-pdf.sh: Likewise.
    * nclcomposer-tutorial.asciidoc: Likewise.

--- generate-pdf.sh
+++ generate-pdf.sh
@@ -1,4 +1,6 @@
 #/bin/bash
+a2x -fpdf -dbook nclcomposer-tutorial.asciidoc
+
 xsltproc /usr/share/xml/docbook/stylesheet/docbook-xsl-ns/fo/docbook.xsl \
 nclcomposer-tutorial.xml > nclcomposer-tutorial.fo
 
