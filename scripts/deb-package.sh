67cd80b116396133b21d2dfea40200dd0dbc5678
Roberto Azevedo <robertogerson@telemidia.puc-rio.br>
2012-12-06 23:56:38 -0200

    [all] maint: Update DEB stuffs w.r.t. 0.1.4.
    * debian/changelog: Likewise.
    * debian/control: Likewise.
    * debian/rules: Likewise.
    * scripts/deb-package.sh: Likewise.

--- scripts/deb-package.sh
+++ scripts/deb-package.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 set -e
 
+#Getting current version
+VERSION=`git --git-dir=./composer-plugins/.git describe --tags | sed "s/v//g"`
+
+# I caan also FORCE a specific version
+VERSION=0.1.4
+
+QMAKE="qmake FORCERELEASE=true RUNSSH=true CPRVERSION=${VERSION} PREFIX=/usr"
+
 #We need to install before to be able to compile the plugins.
-qmake FORCERELEASE=true PREFIX=/usr && make install
+${QMAKE} && make install
 
 #Now, we start to generate the deb.
 debuild -us -uc
 debuild -S -us -uc
-pbuilder build ../composer_0.1.3.dsc
+pbuilder build ../composer_${VERSION}.dsc
 
 #Remove the previous installed NCL Composer
 make uninstall && make clean && make distclean
