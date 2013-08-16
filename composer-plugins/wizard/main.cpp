b0594e6d9dfdce9a04b8268d01bc2c1b6787d18f
Roberto Azevedo <robertogerson@telemidia.puc-rio.br>
2013-08-16 14:37:29 -0300

    [plugins] maint: Generating wizard files from template base.
    * wizard/main.cpp: Now, in order to generate wizard files from template base
      you must run WizardEngine passing the template base as parameter.

--- composer-plugins/wizard/main.cpp
+++ composer-plugins/wizard/main.cpp
@@ -1,6 +1,7 @@
 #include "xmetawizardmainwindow.h"
 #include "wizardexecutionengine.h"
 #include "wizardchooser.h"
+#include "wizardgenerator.h"
 
 #include <QApplication>
 #include <QDebug>
@@ -10,9 +11,18 @@
 int main(int argc, char *argv[])
 {
   QApplication a(argc, argv);
+
+  if(argc > 0) // Generate the .ws from the template base passed as parameter
+  {
+    WizardGenerator::run(argv[1]);
+    return 0;
+  }
+
   wizardchooser w;
   int resp = w.exec("/home/roberto/XWizardOutput/");
+
   qDebug() << resp;
+
   if (resp == QDialog::Accepted)
   {
     WizardExecutionEngine we;
