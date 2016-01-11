4be1442249edaa027b2fbf1dee82495eb67998e6
Eduardo Cruz Araújo <edcaraujo@gmail.com>
2016-01-11 09:05:36 -0200

    build: Update release scripts for MacOS.
    	* macosx/create-release.sh: likewise.
    	* macosx/deploy-release.sh: likewise.

--- macosx/create-release.sh
+++ macosx/create-release.sh
@@ -10,67 +10,22 @@ fi
 
 DIR=`pwd`; DIR="$DIR/../"; cd $DIR;
 
-echo "(1/4) ------ Uninstall ------";
-echo "Uninstalling 'core'...";
-cd $DIR/composer-core 
-sudo make uninstall 
-echo "Uninstalling 'gui'...";
-cd $DIR/composer-gui
-sudo make uninstall 
-echo "Uninstalling 'plugins'...";
-cd $DIR/composer-plugins
+echo "(1/3) ------ Uninstall ------";
+echo "Uninstalling 'all'...";
 sudo make uninstall 
 echo;
 
-echo "(2/4) ------ Clean ------";
-echo "Cleaning 'core'...";
-cd $DIR/composer-core
-sudo make distclean 
-echo "Cleaning 'gui'...";
-cd $DIR/composer-gui
-sudo make ditclean 
-sudo rm -Rf Composer.app
-echo "Cleaning 'plugins'...";
-cd $DIR/composer-plugins
+echo "(2/3) ------ Clean ------";
+echo "Cleaning 'all'...";
 sudo make distclean 
 echo;
 
-echo "(3/4) ------ Update ------";
-echo "Updating 'core'...";
-cd $DIR/composer-core
-git pull 
-
-echo "Cleaning 'gui'...";
-cd $DIR/composer-gui
-git pull 
-echo "Cleaning 'plugins'...";
-cd $DIR/composer-plugins
-git pull  
-echo;
-
-echo "(4/4) ------ Install ------";
-
-echo "Installing release version of 'core'...";
-cd $DIR/composer-core
-qmake -spec macx-g++ composer-core.pro FORCERELEASE=true
+echo "(3/3) ------ Install ------";
+echo "Installing release version of 'all'...";
+qmake -spec macx-g++ nclcomposer.pro FORCERELEASE=true RUNSSHON=false CPRVERSION=$VERSION
+lupdate nclcomposer.pro 
+lrelease nclcomposer.pro
 sudo make
 sudo make install  
-
-echo "Installing release version of 'gui'...";
-cd $DIR/composer-gui
-qmake -spec macx-g++ Composer.pro FORCERELEASE=true RUNSSHON=true CPRVERSION=$VERSION
-lupdate Composer.pro 
-lrelease Composer.pro
-sudo make
-sudo make install  
-
-echo "Installing release version of 'plugins'...";
-cd $DIR/composer-plugins
-qmake -spec macx-g++ plugin-suite.pro FORCERELEASE=true CPRVERSION=$VERSION
-lupdate plugin-suite.pro
-lrelease plugin-suite.pro
-sudo make
-sudo make install  
-echo;
 
 echo Done.
