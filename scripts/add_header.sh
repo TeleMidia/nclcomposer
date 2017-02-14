bc632fc269ad0d2c564cee40565c94ef509dff6e
Roberto Azevedo <robertogerson@telemidia.puc-rio.br>
2017-02-14 10:02:18 +0100

    [scripts] Update add_header script
    * scripts/add_header.sh: Likewise.
    * scripts/header_lgpl.conf: Likewise.

--- scripts/add_header.sh
+++ scripts/add_header.sh
@@ -14,37 +14,30 @@ FIND_PATH=$1     # Where to find the files?
 LICENSE_FILE=$2  # Which header content to put in the file?
                  # This must be a text file
 
-#if license file doesn't exist do nothing!
-if [ -f $LICENSE_FILE ]; then
-  echo "LICENSE FILE DOESN'T EXIST!"
-  exit
-fi
-
-files=`find $1 -path '*ncl-text-editor/deps' -prune -o -path \
-      '*content-provider' -prune -o -path '*composer-gui/deps' -prune -o -name '*.h' -or -name '*.cpp'`
-
+files=`find $1 -name '*.h' -or -name '*.cpp'`
 
 #for each file add the head form head.conf
 for file in $files; do
   echo $file
   
   # Test if the License HEAD is Already on the file.
-  if head $file -n 9 | grep "Eclipse Public License" >/dev/null; then 
-    echo "... File Already with EPL License"; 
-    echo "... Removing previous EPL License";
-    if [ $ISTEST -eq 0 ]; then
-      sed -i '1,9d' $file
-    fi
-  elif head $file -n 9 | grep "" >/dev/null; then
-    echo "... File Already with LGPL License"
-    echo "... Removing LGPL License"
-    if [ $ISTEST -eq 0 ]; then
-      sed -i '1,17d' $file
-    fi
-  fi
+  # if head $file -n 9 | grep "Eclipse Public License" >/dev/null; then
+  #   echo "... File Already with EPL License"; 
+  #   echo "... Removing previous EPL License";
+  #   if [ $ISTEST -eq 0 ]; then
+  #     sed -i '1,9d' $file
+  #   fi
+  # elif head $file -n 9 | grep "" >/dev/null; then
+  #   echo "... File Already with LGPL License"
+  #   echo "... Removing LGPL License"
+  #   if [ $ISTEST -eq 0 ]; then
+  #     sed -i '1,17d' $file
+  #   fi
+  # fi
   if [ $ISTEST -eq 0 ]; then   
+    sed -i '1,15d' $file
     echo ".. Adding $LICENSE_FILE LICENSE HEADER."
-    cat ./scripts/$LICENSE_FILE >/tmp/file
+    cat $LICENSE_FILE >/tmp/file
     cat $file >> /tmp/file
     cp /tmp/file $file
   fi
